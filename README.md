cdb891d91736f10a75c401fbac79b3b02034eb33
x3z3 <avemulapalli@umass.edu>
2022-12-24 08:58:59 -0500

    Created the encoder and decoder files. Check readme for working.

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # image-txt
 Encoder and Decoder to convert an image into text.
 
+## Working
+
+### Encoder
+
+Name the image that you want to encode as sample_encoded.jpg (or alternatively, rename the original file in encoder.py). Run the program, and wait for it to produce the encoded.txt file.
+
+### Decoder
+
+Make sure you have the encoded.txt file in same directory as decoder.py. Run the program, and wait for the sample_decoded.jpg to be produced.
+
+
 ## Motivation
 
 Imagine finding yourself in a flight where you can send unlimited text messages through social media apps, but you aren't allowed to send images. This image encoder can help you break your image into a series of text messages (which you could send through the app) which can finally be decoded to convert the text messages back into the original image.
---
+++ encoder.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+from PIL import Image
+
+SAMPLE_IMAGE_PATH = "sample.jpg"
+ENCODED_IMAGE_PATH = "sample_encoded.jpg"
+
+STRING_MAP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+INT_MAP = {STRING_MAP[i]: i for i in range(len(STRING_MAP))}
+
+def convertToChar(value):
+    value = value//4
+    return STRING_MAP[value]
+
+def convertToValue(char):
+    return INT_MAP[char]
+
+def writeToFile(string):
+    with open("encoded.txt", "a") as f:
+        f.write(string)
+
+def main():
+    if os.path.exists("encoded.txt"):
+        os.remove("encoded.txt")
+    else:
+        print("The file does not exist")
+
+    p = Image.open(SAMPLE_IMAGE_PATH)
+    p = p.convert(mode="RGB")
+    p.thumbnail(size=(480, 270))
+    image = np.array(p).astype(int)
+
+    string = ""
+    for row in range(image.shape[0]):
+        for col in range(image.shape[1]):
+            for channel in range(image.shape[2]):
+                char = str(convertToChar(image[row, col, channel]))
+                string += char
+                if len(string) == 4:
+                    writeToFile(string)
+                    string = ""
+    
+    p = Image.fromarray(image.astype(np.uint8))
+    p.save(ENCODED_IMAGE_PATH)
+
+
+
+if __name__ == "__main__":
+    main()
+
